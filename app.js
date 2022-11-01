9558709a71c27fb32a19deea83c9b667e0f55a8c
Jessica Mosby <jessicamosby@mac.com>
2022-11-01 14:19:04 -0400

    defined consts in dom

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
 /* Imports */
 
 /* Get DOM Elements */
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
+
+const img1 = document.getElementById('#img2');
+const img2 = document.getElementById('#img2');
+const img3 = document.getElementById('#img3');
+
+const winsEL = document.getElementById('wins');
+const lossesEL = document.getElementById('losses');
+const totalEL = document.getElementById('total');
 
 /* State */
+let wins = 0;
+let total = 0;
 
 /* Events */
+button1.addEventListener('click', () => {
+    total++;
+     const 
+});
 
 /* Display Functions */
 
